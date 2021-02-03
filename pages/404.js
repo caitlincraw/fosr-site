f4f2825da6ef07f709e33d07406bfce17f8716d2
cooksey <suitcase24@gmail.com>
2021-02-02 22:41:23 -0500

    seo

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+    return (
+        <main>
+            <h1>404 - Page Not Found</h1>
+            <p>Did you get lost on Gig City again?</p>
+        </main>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
         <Head>
-        <link rel="shortcut icon" href="./favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet"></link>
-        <title>Friends of Stringer's Ridge</title>
+          <link rel="shortcut icon" href="./favicon.ico" />
+          <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet"></link>
+          <title>Friends of Stringer's Ridge</title>
+          <meta property="og:title" content="Friends of Stringer's Ridge" />
+          <meta name="description" content="The Friends of String's Ridge is a group dedicated to supporting and protecting Stringer's Ridge park in Chattanooga Tennessee." />
+          <meta name="og:description" content="The Friends of String's Ridge is a group dedicated to supporting and protecting Stringer's Ridge park in Chattanooga Tennessee." />
+          <meta name="robots" content="index, follow" />
         </Head>
         <Component {...pageProps} />
     </Layout>
