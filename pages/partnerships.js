1359c08f5fcea06d63ab5a01d1834c9009069d41
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-02-16 22:01:21 -0500

    The city's website ain't secure

--- pages/partnerships.js
+++ pages/partnerships.js
@@ -6,7 +6,7 @@ export default function Partnerships() {
         <main>
           <h1>Partnerships</h1>
           <div className={ styles.container }>
-            <Partnership site="https://www.chattanooga.gov/parks" logo="images/cityofchattlogo.png" alt="City of Chattanooga Logo" />
+            <Partnership site="http://www.chattanooga.gov/parks" logo="images/cityofchattlogo.png" alt="City of Chattanooga Logo" />
             <Partnership site="https://www.trgt.org/stringers" logo="images/trgtlogo.png" alt="Tennessee River Gorge Trust (TRGT) Logo" />
             <Partnership site="https://www.tpl.org/our-work/stringers-ridge" logo="images/tpllogo.png" alt="Trust for Public Land (TPL) Logo" />
             <Partnership site="https://www.sorbachattanooga.org/" logo="images/sorbalogo.png" alt="Southern Off-Road Bicycle Association (SORBA) Logo" />
